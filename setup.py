9b171f857a3e268925b2d86d67c21c679a87a7a0
AhmadAlhaj7 <ahmad.alhaja79@gmail.com>
2025-11-11 16:44:34 +0400

    requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ahmad",
+    packages=find_packages(),
+    author_email="ahmad.alhaja79@gmail.com",
+    install_requires=[]
+)
